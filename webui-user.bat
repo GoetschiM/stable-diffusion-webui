78bb46091df64517db9d0f338cab5dcf501fe2a6
GoetschiM <michelgoetschi@gmail.com>
2026-02-16 22:41:59 +0100

    hmm

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\ProgramData\miniconda3\envs\sd\python.exe
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=-
+set COMMANDLINE_ARGS=--listen --port 7860 --api --cors-allow-origins=* --xformers --medvram
 
 call webui.bat
